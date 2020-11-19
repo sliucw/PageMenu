195172c43b468d4072f3698ed5cfa65987dae036
sliucw <sliucw@gmail.com>
2020-11-19 11:43:43 +0800

    feat: 对外公开scrollView

--- Classes/CAPSPageMenu.swift
+++ Classes/CAPSPageMenu.swift
@@ -33,8 +33,8 @@ open class CAPSPageMenu: UIViewController {
     
     // MARK: - Properties
 
-    let menuScrollView = UIScrollView()
-    let controllerScrollView = UIScrollView()
+    public let menuScrollView = UIScrollView()
+    public let controllerScrollView = UIScrollView()
     var controllerArray : [UIViewController] = []
     var menuItems : [MenuItemView] = []
     var menuItemWidths : [CGFloat] = []
